29b1675bb0fd209a3331484f9a0e9f52b21b9193
lucian2302 <45819589+lucian2302@users.noreply.github.com>
2018-12-12 13:29:09 +0100

    version 0.1.1

--- java/src/evaluacion1/holagit.java
+++ java/src/evaluacion1/holagit.java
@@ -4,7 +4,7 @@ public class holagit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("holagit");
+		System.out.println("holagit2");
 	}
 
 }
